9e6a6151c83c2a3520ebe24f1503d7a4a2cb433b
lucascdornelas <lucascdornelas@gmail.com>
2022-12-11 10:37:34 -0300

    fix: create public layout for home and gallery page

---
+++ front-end/src/components/public-navbar.js
@@ -0,0 +1,62 @@
+import { useRef, useState } from "react";
+import PropTypes from "prop-types";
+import styled from "@emotion/styled";
+import { AppBar, Avatar, Badge, Box, Button, IconButton, Toolbar, Tooltip } from "@mui/material";
+import { UserCircle as UserCircleIcon } from "../icons/user-circle";
+import { useRouter } from "next/router";
+import { Logo } from "./logo";
+import NextLink from "next/link";
+
+const PublicNavbarRoot = styled(AppBar)(({ theme }) => ({
+  backgroundColor: theme.palette.background.paper,
+  boxShadow: theme.shadows[3],
+}));
+
+export const PublicNavbar = (props) => {
+  const { onSidebarOpen, ...other } = props;
+  const settingsRef = useRef(null);
+
+  const router = useRouter();
+
+  const handleGoToLoginPage = () => {
+    router.push("/login");
+  };
+
+  const handleGoToGallery = () => {
+    router.push("/gallery");
+  };
+
+  return (
+    <>
+      <PublicNavbarRoot>
+        <Toolbar
+          disableGutters
+          sx={{
+            minHeight: 64,
+            left: 0,
+            px: 2,
+          }}
+        >
+          <NextLink href="/home" passHref>
+            <Logo variant="light" />
+          </NextLink>
+
+          <Button color="secondary" onClick={handleGoToGallery} style={{ marginLeft: "1rem" }}>
+            Galeria
+          </Button>
+          <Button color="secondary">Marcar Consulta</Button>
+
+          <Box sx={{ flexGrow: 1 }} />
+
+          <Button color="primary" onClick={handleGoToLoginPage}>
+            Área Interna
+          </Button>
+        </Toolbar>
+      </PublicNavbarRoot>
+    </>
+  );
+};
+
+PublicNavbar.propTypes = {
+  onSidebarOpen: PropTypes.func,
+};
--- front-end/src/pages/login.js
+++ front-end/src/pages/login.js
@@ -1,35 +1,24 @@
-import Head from 'next/head';
-import NextLink from 'next/link';
-import Router from 'next/router';
-import { useFormik } from 'formik';
-import * as Yup from 'yup';
-import { Box, Button, Container, Grid, Link, TextField, Typography } from '@mui/material';
-import ArrowBackIcon from '@mui/icons-material/ArrowBack';
-import { Facebook as FacebookIcon } from '../icons/facebook';
-import { Google as GoogleIcon } from '../icons/google';
+import Head from "next/head";
+import NextLink from "next/link";
+import Router from "next/router";
+import { useFormik } from "formik";
+import * as Yup from "yup";
+import { Box, Button, Container, Grid, Link, TextField, Typography } from "@mui/material";
+import ArrowBackIcon from "@mui/icons-material/ArrowBack";
 
 const Login = () => {
   const formik = useFormik({
     initialValues: {
-      email: 'demo@devias.io',
-      password: 'Password123'
+      email: "demo@devias.io",
+      password: "Password123",
     },
     validationSchema: Yup.object({
-      email: Yup
-        .string()
-        .email('Must be a valid email')
-        .max(255)
-        .required('Email is required'),
-      password: Yup
-        .string()
-        .max(255)
-        .required('Password is required')
+      email: Yup.string().email("Must be a valid email").max(255).required("Email is required"),
+      password: Yup.string().max(255).required("Password is required"),
     }),
     onSubmit: () => {
-      Router
-        .push('/')
-        .catch(console.error);
-    }
+      Router.push("/patients").catch(console.error);
+    },
   });
 
   return (
@@ -40,91 +29,28 @@ const Login = () => {
       <Box
         component="main"
         sx={{
-          alignItems: 'center',
-          display: 'flex',
+          alignItems: "center",
+          display: "flex",
           flexGrow: 1,
-          minHeight: '100%'
+          minHeight: "100%",
         }}
       >
         <Container maxWidth="sm">
-          <NextLink
-            href="/"
-            passHref
-          >
-            <Button
-              component="a"
-              startIcon={<ArrowBackIcon fontSize="small" />}
-            >
-              Dashboard
+          <NextLink href="/home" passHref>
+            <Button component="a" startIcon={<ArrowBackIcon fontSize="small" />}>
+              Home
             </Button>
           </NextLink>
           <form onSubmit={formik.handleSubmit}>
             <Box sx={{ my: 3 }}>
-              <Typography
-                color="textPrimary"
-                variant="h4"
-              >
-                Sign in
+              <Typography color="textPrimary" variant="h4">
+                Entrar
               </Typography>
-              <Typography
-                color="textSecondary"
-                gutterBottom
-                variant="body2"
-              >
-                Sign in on the internal platform
+              <Typography color="textSecondary" gutterBottom variant="body2">
+                Área Interna
               </Typography>
             </Box>
-            <Grid
-              container
-              spacing={3}
-            >
-              <Grid
-                item
-                xs={12}
-                md={6}
-              >
-                <Button
-                  color="info"
-                  fullWidth
-                  startIcon={<FacebookIcon />}
-                  onClick={() => formik.handleSubmit()}
-                  size="large"
-                  variant="contained"
-                >
-                  Login with Facebook
-                </Button>
-              </Grid>
-              <Grid
-                item
-                xs={12}
-                md={6}
-              >
-                <Button
-                  color="error"
-                  fullWidth
-                  onClick={() => formik.handleSubmit()}
-                  size="large"
-                  startIcon={<GoogleIcon />}
-                  variant="contained"
-                >
-                  Login with Google
-                </Button>
-              </Grid>
-            </Grid>
-            <Box
-              sx={{
-                pb: 1,
-                pt: 3
-              }}
-            >
-              <Typography
-                align="center"
-                color="textSecondary"
-                variant="body1"
-              >
-                or login with email address
-              </Typography>
-            </Box>
+
             <TextField
               error={Boolean(formik.touched.email && formik.errors.email)}
               fullWidth
@@ -160,30 +86,9 @@ const Login = () => {
                 type="submit"
                 variant="contained"
               >
-                Sign In Now
+                Entrar
               </Button>
             </Box>
-            <Typography
-              color="textSecondary"
-              variant="body2"
-            >
-              Don&apos;t have an account?
-              {' '}
-              <NextLink
-                href="/register"
-              >
-                <Link
-                  to="/register"
-                  variant="subtitle2"
-                  underline="hover"
-                  sx={{
-                    cursor: 'pointer'
-                  }}
-                >
-                  Sign Up
-                </Link>
-              </NextLink>
-            </Typography>
           </form>
         </Container>
       </Box>
