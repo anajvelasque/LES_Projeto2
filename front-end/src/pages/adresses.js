bb8bc0a476dd3f3959d8e92af7bc9718784d52a3
lucascdornelas <lucascdornelas@gmail.com>
2022-12-11 10:01:47 -0300

    feat: create crud to entity address

--- front-end/src/pages/adresses.js
+++ front-end/src/pages/adresses.js
@@ -1,32 +1,50 @@
 import Head from "next/head";
 import { Box, Container } from "@mui/material";
-import { CustomerListResults } from "../components/customer/customer-list-results";
 import { DashboardLayout } from "../components/dashboard-layout";
-import { customers } from "../__mocks__/customers";
 import { AddressesListToolbar } from "../components/addresses/addresses-list-toolbar";
+import { AddressesListResults } from "../components/addresses/addresses-list-results";
+import { axiosClient } from "../utils/axiosClient";
 
-const Page = () => (
-  <>
-    <Head>
-      <title>Endereços</title>
-    </Head>
-    <Box
-      component="main"
-      sx={{
-        flexGrow: 1,
-        py: 8,
-      }}
-    >
-      <Container maxWidth={false}>
-        <AddressesListToolbar />
-        <Box sx={{ mt: 3 }}>
-          <CustomerListResults customers={customers} />
-        </Box>
-      </Container>
-    </Box>
-  </>
-);
+async function getAddressesAsync() {
+  const result = await axiosClient.get("/cep/");
+
+  return result.data?.result || [];
+}
+
+const Page = ({ addresses }) => {
+  return (
+    <>
+      <Head>
+        <title>Endereços</title>
+      </Head>
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          py: 8,
+        }}
+      >
+        <Container maxWidth={false}>
+          <AddressesListToolbar />
+          <Box sx={{ mt: 3 }}>
+            <AddressesListResults addresses={addresses} />
+          </Box>
+        </Container>
+      </Box>
+    </>
+  );
+};
 
 Page.getLayout = (page) => <DashboardLayout>{page}</DashboardLayout>;
 
 export default Page;
+
+export async function getServerSideProps(context) {
+  try {
+    const addresses = await getAddressesAsync();
+
+    return { props: { addresses } };
+  } catch (error) {
+    return { props: { addresses: [] } };
+  }
+}
